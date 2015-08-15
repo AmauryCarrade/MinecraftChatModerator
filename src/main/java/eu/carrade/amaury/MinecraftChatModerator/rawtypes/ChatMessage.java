1264b240834f7272d1d11d2529843f7a64bd4a4e
Amaury Carrade <amaury@carrade.eu>
2015-08-15 03:30:59 +0200

    Added data structure
    * NEW: Added a `ChatMessage` object to store messages sent.
    * NEW: Added a `PlayerChatHistory` object to store the messages a player sent recently.

---
+++ src/main/java/eu/carrade/amaury/MinecraftChatModerator/rawtypes/ChatMessage.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2015 Amaury Carrade
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package eu.carrade.amaury.MinecraftChatModerator.rawtypes;
+
+import java.util.*;
+
+
+public class ChatMessage
+{
+	private UUID sender;
+	private String message;
+	private Long time;
+
+	/**
+	 * Constructs a new chat message.
+	 *
+	 * @param sender The message's sender.
+	 * @param message The message's body.
+	 * @param time The milli-timestamp when the message was sent.
+	 *
+	 * @throws NullPointerException If any of the arguments is null.
+	 */
+	public ChatMessage(UUID sender, String message, Long time)
+	{
+		this.sender = Objects.requireNonNull(sender, "The sender cannot be null!");
+		this.message = Objects.requireNonNull(message, "The message cannot be null!");
+		this.time = Objects.requireNonNull(time, "The time cannot be null!");
+	}
+
+	/**
+	 * Constructs a new chat message sent just now (when the method is called).
+	 *
+	 * @param sender The message's sender.
+	 * @param message The message's body.
+	 *
+	 * @throws NullPointerException If any of the arguments is null.
+	 */
+	public ChatMessage(UUID sender, String message)
+	{
+		this(sender, message, System.currentTimeMillis());
+	}
+
+
+	/**
+	 * Returns the message's sender.
+	 *
+	 * @return The sender.
+	 */
+	public UUID getSender()
+	{
+		return sender;
+	}
+
+	/**
+	 * Returns the message's body.
+	 *
+	 * @return The message.
+	 */
+	public String getMessage()
+	{
+		return message;
+	}
+
+	/**
+	 * Returns the message's sending time.
+	 *
+	 * @return The milli-timestamp when the message was sent.
+	 */
+	public Long getTime()
+	{
+		return time;
+	}
+
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o) return true;
+		if (!(o instanceof ChatMessage)) return false;
+
+		ChatMessage that = (ChatMessage) o;
+
+		return sender.equals(that.sender) && message.equals(that.message) && time.equals(that.time);
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(sender, message, time);
+	}
+
+	@Override
+	public String toString()
+	{
+		return "ChatMessage{" +
+				"sender=" + sender +
+				", message='" + message + '\'' +
+				", time=" + time + '}';
+	}
+}
---
+++ src/main/java/eu/carrade/amaury/MinecraftChatModerator/rawtypes/PlayerChatHistory.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2015 Amaury Carrade
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package eu.carrade.amaury.MinecraftChatModerator.rawtypes;
+
+import java.util.*;
+
+
+public class PlayerChatHistory
+{
+	private static final Long MAXIMUM_MESSAGE_AGE = 120000l; // ms (120'000 = 2 minutes)
+
+	private UUID playerID;
+	private Set<ChatMessage> messages = new HashSet<>();
+
+
+	/**
+	 * Constructs a new chat history belonging to the given player.
+	 *
+	 * @param playerID The history's owner.
+	 *
+	 * @throws NullPointerException If the {@code playerID} is {@code null}.
+	 */
+	public PlayerChatHistory(UUID playerID)
+	{
+		this.playerID = Objects.requireNonNull(playerID, "The player UUID cannot be null!");
+	}
+
+
+	/**
+	 * Adds a message to this chat history.
+	 *
+	 * @param message The message to add.
+	 *
+	 * @throws IllegalArgumentException If the message does not belong to the owner of this history.
+	 */
+	public void addMessage(ChatMessage message)
+	{
+		if(!message.getSender().equals(playerID))
+			throw new IllegalArgumentException("Cannot add a message to this player history, UUIDs mismatch! (" + message.getSender() + " (message) versus " + playerID + "(history).)");
+
+		messages.add(message);
+	}
+
+
+	/**
+	 * Forgets all messages older than {@link #MAXIMUM_MESSAGE_AGE} ms.
+	 */
+	public void cleanup()
+	{
+		Long now = System.currentTimeMillis();
+
+		new HashSet<>(messages).stream()
+				.filter(message -> message.getTime() < now - MAXIMUM_MESSAGE_AGE)
+				.forEach(messages::remove);
+	}
+
+
+	/**
+	 * Returns the history's owner's UUID.
+	 *
+	 * @return The UUID.
+	 */
+	public UUID getPlayerID()
+	{
+		return playerID;
+	}
+
+	/**
+	 * Returns the stored messages.
+	 *
+	 * @return The messages.
+	 */
+	public Set<ChatMessage> getMessages()
+	{
+		return messages;
+	}
+
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o) return true;
+		if (!(o instanceof PlayerChatHistory)) return false;
+
+		PlayerChatHistory that = (PlayerChatHistory) o;
+
+		return playerID.equals(that.playerID) && messages.equals(that.messages);
+
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(playerID, messages);
+	}
+}
